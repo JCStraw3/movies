61c0b81821c6382bf8783b148819d3ad3eff97c5
Jessica Straw <jcstraw3@gmail.com>
2015-10-07 20:05:05 -0400

    Added Javascript/jQuery plugin - dismissible alerts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,11 @@
 
 <head>
 	<title>Movies - @yield('title')</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="/css/app.css">
+	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
+	<link rel='stylesheet' type='text/css' href='/css/app.css'>
+	<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 </head>
 
 <body>
