c078612110aa25c81b348b1c6d82c7565348b302
Jessica Straw <jcstraw3@gmail.com>
2015-10-08 22:51:18 -0400

    Removed UpdateMovieRequest rules

--- app/Http/Requests/UpdateMovieRequest.php
+++ app/Http/Requests/UpdateMovieRequest.php
@@ -23,12 +23,6 @@ class UpdateMovieRequest extends Request
     public function rules(){
         return [
             'title' => 'required',
-            'genre' => 'required',
-            'rating' => 'required',
-            'director' => 'required',
-            'writer' => 'required',
-            'cast' => 'required',
-            'label' => 'required',
         ];
     }
 
