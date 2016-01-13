def8d9627a824aacdff1d99dd8ac9fe76ab84016
Jessica Straw <jcstraw3@gmail.com>
2016-01-12 23:05:57 -0500

    Adds auth middleware to userController so anyone not logged in gets redirected to login page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,6 +12,14 @@ use Uuid;
 
 class UserController extends Controller {
 
+// Middleware
+
+	// Only authenticated users can see certain pages.
+
+	public function __construct(){
+		$this->middleware('auth');
+	}
+
 // Views.
 
 	// View a user's profile.
