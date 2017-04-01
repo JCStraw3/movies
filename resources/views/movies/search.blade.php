573acf79bffa7282c5dce4ec0489d9badb08944d
Jessica Straw <jcstraw3@gmail.com>
2017-04-01 19:25:23 -0400

    Removes popover in movies search and replaces it with model

--- resources/views/movies/search.blade.php
+++ resources/views/movies/search.blade.php
@@ -41,7 +41,9 @@
 
 				@if($movie->note)
 					<div>
-						<a tabindex='0' class='btn btn-xs btn-primary pull-right' role='button' data-toggle='popover' data-trigger='focus' data-content='{{ $movie->note }}'><span class='glyphicon glyphicon-paperclip'></span></a>
+						<button type='button' class='btn btn-xs btn-primary pull-right' data-toggle='modal' data-target='#{{ $movie->id }}'>
+							<span class='glyphicon glyphicon-paperclip'></span>
+						</button>
 					</div>
 				@endif
 
@@ -150,15 +152,31 @@
 			
 		</div>
 
+		{{-- Note Modal --}}
+
+		<div class='modal fade' id='{{ $movie->id }}' tabindex='-1' role='dialog' aria-labelledby='noteModal'>
+
+			<div class='modal-dialog' role='document'>
+
+				<div class='modal-content'>
+
+					<div class='modal-header'>
+						<button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+						
+						<h4 class='modal-title' id='noteModal'>{{ $movie->title }}</h4>
+					</div>
+
+					<div class='modal-body'>
+						{{ $movie->note }}
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
 	@endforeach
-
-	{{-- Popover Script --}}
-
-	<script>
-		$(function () {
-			$('[data-toggle="popover"]').popover()
-		})
-	</script>
 
 	{{-- Ajax delete movie --}}
 
